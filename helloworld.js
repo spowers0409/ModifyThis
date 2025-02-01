515940d7d103cb4fb902113f9a049d87ef9bc7cc
Sam Powers <odinissecure6@outlook.com>
2025-01-31 20:45:06 -0600

    Added week 3 text to output

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World my name is Odin\n')
+   response.end('Hello World my name is Odin, and this is week 3!\n')
 }).listen(3333)
 
 // Console will print the message
